cc3069e9dc2327ec920b5c1c5e640179bf1b44f0
Lyon Liang <lyon.liang12@gmail.com>
2020-02-24 23:16:12 +0800

    feat: load MDX

---
+++ packages/gatsby-theme-docs/gatsby-config.js
@@ -0,0 +1,28 @@
+// Since this is the theme, so get default options firstly
+const withDefaults = require('./utils/default-options');
+
+// not return object, but tailor it based on options
+module.exports = options => {
+  const { contentPath, useExternalMDX } = withDefaults(options);
+
+  return {
+    plugins: [
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          // to avoid name confiliction
+          name: 'gatsby-source-filesystem',
+          path: contentPath,
+        },
+      },
+      !useExternalMDX && {
+        resolve: 'gatsby-plugin-mdx',
+        options: {
+          defaultLayouts: {
+            default: require.resolve('./src/components/layout.js'),
+          },
+        },
+      },
+    ].filter(Boolean), // since useExternalMDX maybe true, so use filter to get rid of this Boolean value
+  };
+};
